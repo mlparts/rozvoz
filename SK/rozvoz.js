3c7e50d3e58f79876fb8afa2fc8a10b660e120b4
mlparts <69846990+mlparts@users.noreply.github.com>
2021-04-15 17:50:37 +0200

    Add files via upload

---
+++ SK/rozvoz.js
@@ -0,0 +1,2 @@
+$(".layout-col-content").html('<div class="custom-content"><div class="container"><div class="c-page-heading"><h1>Rozvoz pre servisy</h1></div><div class="c-alert c-alert-info"> <img src="https://mlparts.github.io/rozvoz/icon-info.svg"> V Brne je pre autoservisy rozvoz ZADARMO</div><div class="c-page-map"> <img src="https://mlparts.github.io/rozvoz/rozvoz-mapa.png"></div></div></div>');
+
